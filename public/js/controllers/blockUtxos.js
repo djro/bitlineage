72cf306b1e98bd3217765fe4916931d6b06420fd
djro <12958052+djro@users.noreply.github.com>
2018-03-09 00:03:38 +0700

    added blockchaininfo webapi service

--- public/js/controllers/blockUtxos.js
+++ public/js/controllers/blockUtxos.js
@@ -1,19 +1,28 @@
 angular.module('blockUtxosController', [])
 
-.controller('blockUtxosController', function($scope, $http, BlockHeight, BlockSearchHistory){
+.controller('blockUtxosController', function($scope, $http, BlockHeight, BlockSearchHistory, BlockChainInfo){
     this.getBlockFromNumber = function(blockNum){
         vm = this;
         BlockHeight.get(blockNum)
         .then(function(data) {
+            
             vm.blockHash = data.data.blockhash || data.data.data.blockhash;
+            $scope.$apply();
 
             BlockSearchHistory.create(blockNum, vm.blockHash, new Date())
-            .then(function(data){
-
+            .then(function(result){
+               
             },function(err){
 
             });
-            $scope.$apply();
+
+            BlockChainInfo.getBlockByHash(vm.blockHash)
+            .then(function(result){
+                vm.blockInfo = result.data;
+            },function(err){
+                var ex = error;
+            });
+            
         },
         function(error){
             var ex = error;
