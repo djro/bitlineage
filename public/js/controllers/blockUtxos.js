6d1ab4fbd94eb3c453e7e46fce1866abb77fd31b
djro <djro@email.com>
2018-03-08 18:27:49 +0700

    added blocksearchhistory

--- public/js/controllers/blockUtxos.js
+++ public/js/controllers/blockUtxos.js
@@ -1,11 +1,18 @@
 angular.module('blockUtxosController', [])
 
-.controller('blockUtxosController', function($scope, $http, BlockHeight){
+.controller('blockUtxosController', function($scope, $http, BlockHeight, BlockSearchHistory){
     this.getBlockFromNumber = function(blockNum){
         vm = this;
         BlockHeight.get(blockNum)
         .then(function(data) {
             vm.blockHash = data.data.blockhash || data.data.data.blockhash;
+
+            BlockSearchHistory.create(blockNum, vm.blockHash, new Date())
+            .then(function(data){
+
+            },function(err){
+
+            });
             $scope.$apply();
         },
         function(error){
