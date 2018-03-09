2ff5e0cbc726f21d2c6a0c54605eaac31130703a
djro <12958052+djro@users.noreply.github.com>
2018-03-09 16:14:02 +0700

    upadated angurlar service to include get rawaddress,updated route urls

--- public/js/services/blockchaininfo.js
+++ public/js/services/blockchaininfo.js
@@ -4,7 +4,11 @@ app.factory('BlockChainInfo', function($http){
     return {
         getBlockByHash: function (hash){
             //hash='000000000000000002cce816c0ab2c5c269cb081896b7dcb34b8422d6b74ffa1';
-            return $http.get('/api/blockchaininfo/' + hash);
+            return $http.get('/api/blockchaininfo/rawblock/' + hash);
+        }
+        ,getRawAddress: function(address){
+            //address = '1AJbsFZ64EpEfS5UAjAfcUG8pH8Jn3rn1F';
+            return $http.get('/api/blockchaininfo/rawaddr/' + address);
         }
     }
 });
