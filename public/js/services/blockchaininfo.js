9229d08bf248c93d3a7dd6bd04f459380e757ae1
djro <12958052+djro@users.noreply.github.com>
2018-03-10 17:36:38 +0700

    external js/css now included/ client code cleanup

--- public/js/services/blockchaininfo.js
+++ public/js/services/blockchaininfo.js
@@ -4,11 +4,17 @@ app.factory('BlockChainInfo', function($http){
     return {
         getBlockByHash: function (hash){
             //hash='000000000000000002cce816c0ab2c5c269cb081896b7dcb34b8422d6b74ffa1';
-            return $http.get('/api/blockchaininfo/rawblock/' + hash);
+            return $http.get('/api/blockchaininfo/rawblock/' + hash)
+            .then(function(response){
+                return response.data
+            })
         }
         ,getRawAddress: function(address){
             //address = '1AJbsFZ64EpEfS5UAjAfcUG8pH8Jn3rn1F';
-            return $http.get('/api/blockchaininfo/rawaddr/' + address);
+            return $http.get('/api/blockchaininfo/rawaddr/' + address)
+            .then(function(response){
+                return response.data
+            })
         }
     }
 });
--- public/js/services/utxoPreviews.js
+++ public/js/services/utxoPreviews.js
@@ -2,13 +2,22 @@ angular.module('utxoPreviewService', [])
     .factory('UtxoPreviews', function($http) {
         return {
             get : function() {
-                return $http.get('/api/utxoPreviews');
+                return $http.get('/api/utxoPreviews')
+                .then(function(response){
+                    return response.data
+                });
             },
             create : function(utxoPreviewData) {
-                return $http.post('/api/utxoPreviews', utxoPreviewData);
+                return $http.post('/api/utxoPreviews', utxoPreviewData)
+                .then(function(response){
+                    return response.data
+                });
             },
             delete : function(id) {
-                return $http.delete('/api/utxoPreviews/' + id);
+                return $http.delete('/api/utxoPreviews/' + id)
+                .then(function(response){
+                    return response.data
+                })
             }
         }
     });
