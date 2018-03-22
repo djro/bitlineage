fc3a37e90a83d08c083dde58892c99e9f276f8d2
djro <12958052+djro@users.noreply.github.com>
2018-03-22 15:43:49 +0700

    apply filtering to only show spent transactions

--- public/js/controllers/transactionLineage.js
+++ public/js/controllers/transactionLineage.js
@@ -6,7 +6,31 @@ angular.module('transactionLineageController', [])
         vm.getRawAddress = function(address){
             BlockChainInfo.getRawAddress(address).then(function(data){
                 vm.getRawAddress = data;
+
+                var spentTxs = vm.filterSpentTxs(address, data.txs)
             })
+        }
+
+
+        vm.filterSpentTxs = function(parentAddr, txs){
+            var txsToInclude = [];
+            for(var tx of txs){
+                var ammountFromAddress = 0;
+                var totalInputAmmount = 0;
+                for(var txInput of tx.inputs){
+                    if (txInput.prev_out.addr == parentAddr){
+                        ammountFromAddress += txInput.prev_out.value;
+                    } 
+                    totalInputAmmount += txInput.prev_out.value;
+                }
+
+                if(ammountFromAddress >0){
+                    tx.percentageInputsFromParent = (ammountFromAddress / totalInputAmmount) * 100;
+                    txsToInclude.push(tx);
+                }
+            }
+
+            return txsToInclude;
         }
 
     });
